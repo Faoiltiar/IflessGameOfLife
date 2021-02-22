d315e800e2cb83daa23d16a39185ffea3068b51b
Mariusz Raczyk <53018354+Faoiltiar@users.noreply.github.com>
2021-02-22 18:49:07 +0100

    Add javadoc to World class

--- src/main/java/com/raczyk/gameoflife/world/World.java
+++ src/main/java/com/raczyk/gameoflife/world/World.java
@@ -6,17 +6,34 @@ import java.util.Map;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-class World {
+/**
+ * Class representing world build with matrix of cells.
+ */
+public class World {
   private final int maxColumnNo;
   private final int maxRowNo;
   private final Map<Point, Cell> worldMatrix;
 
+  /**
+   * Constructs a world represented as matrix of cells with
+   * given number of columns and rows passed as parameter.
+   *
+   * @param maxColumnNo number of columns in matrix of cells.
+   * @param maxRowNo number of rows in matrix of cells.
+   * @param worldMatrix matrix used as a container for cells.
+   */
   public World(int maxColumnNo, int maxRowNo, Map<Point, Cell> worldMatrix) {
     this.maxColumnNo = maxColumnNo;
     this.maxRowNo = maxRowNo;
     this.worldMatrix = worldMatrix;
   }
 
+  /**
+   * Method initializing world matrix. It firstly create cells and pick random state for each one.
+   * When all cells are created, each cell register its neighbours.
+   *
+   * @param random object used to generate random states of cells.
+   */
   public void initWorld(Random random) {
     initializeWorldWithCells(random);
     registerNeighbour();
@@ -47,6 +64,10 @@ class World {
         );
   }
 
+  /**
+   * Method establishing next generation of cells by setting future state
+   * for each cell in world matrix.
+   */
   public void setNextGeneration() {
     IntStream.range(0, maxColumnNo)
         .forEach(coordinateX -> IntStream.range(0, maxRowNo)
@@ -58,6 +79,10 @@ class World {
         );
   }
 
+  /**
+   * Method evolving cells to the new generation.
+   * It does that by changing current state of each cell to the future state.
+   */
   public void evolveToNextGeneration() {
     IntStream.range(0, maxColumnNo)
         .forEach(coordinateX -> IntStream.range(0, maxRowNo)
@@ -69,6 +94,11 @@ class World {
         );
   }
 
+  /**
+   * Method returning String representing state of cells in world matrix.
+   *
+   * @return String presenting current world state.
+   */
   public String getWorldRepresentation() {
     final var stateSeparator = " ";
     final var rowSeparator = "\n";
